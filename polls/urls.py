d6cec16cdae8c43cd4a48ace7d5eb21f38aea425
skinbone <skinbone@gmail.com>
2012-07-30 10:45:35 +0800

    first commit

---
+++ polls/urls.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf8 -*-
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('polls.views',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^mysite/', include('mysite.foo.urls')),第两个
+    url(r'^$', 'index'),
+    url(r'^(?P<poll_id>\d+)/$', 'detail'),
+    url(r'^(?P<poll_id>\d+)/results/$', 'results'),
+    url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
+)
+
+
